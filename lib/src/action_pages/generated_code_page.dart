15130795c9c4ebd5d2023a997d061439d4ff3d9b
Mike Zamayias <mzamagias@gmail.com>
2021-09-21 20:14:31 +0300

    refactor

--- lib/src/action_pages/generated_code_page.dart
+++ lib/src/action_pages/generated_code_page.dart
@@ -12,8 +12,16 @@ class GeneratedCodePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xfff3f3f3),
       appBar: AppBar(
         centerTitle: true,
+        backgroundColor: const Color(0xfff3f3f3),
+        automaticallyImplyLeading: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_rounded),
+          onPressed: () => Navigator.pop(context),
+          color: const Color(0xff303030),
+        ),
         title: Text(
           'Generated QR Code',
           style: TextStyle(
@@ -22,6 +30,18 @@ class GeneratedCodePage extends StatelessWidget {
             color: const Color(0xff303030),
           ),
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.share_rounded),
+            color: const Color(0xff303030),
+            tooltip: 'Share QR Code',
+            onPressed: () => ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('This feature is under development.'),
+              ),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Padding(
@@ -40,7 +60,6 @@ class GeneratedCodePage extends StatelessWidget {
               : Container(),
         ),
       ),
-      backgroundColor: const Color(0xfff3f3f3),
     );
   }
 }
