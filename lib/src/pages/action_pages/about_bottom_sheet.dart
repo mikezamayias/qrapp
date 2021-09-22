af3301309c56afcae215b4d5f214b2bf2199be93
Mike Zamayias <mzamagias@gmail.com>
2021-09-22 13:04:13 +0300

    began implementaion of about page (on bottom sheet)

--- lib/src/pages/action_pages/about_bottom_sheet.dart
+++ lib/src/pages/action_pages/about_bottom_sheet.dart
@@ -6,8 +6,6 @@ class AboutBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomSheet(
-      onClosing: () {},
-      builder: (_) => Container(),
       enableDrag: false,
       elevation: 9,
       backgroundColor: const Color(0xfff3f3f3),
@@ -15,6 +13,58 @@ class AboutBottomSheet extends StatelessWidget {
         borderRadius: const BorderRadius.only(
           topLeft: const Radius.circular(21),
           topRight: const Radius.circular(21),
+        ),
+      ),
+      onClosing: () {},
+      builder: (_) => Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: Center(
+                child: Text(
+                  'About',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: const Color(0xff303030),
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Author',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: const Color(0xff303030),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Open Source Libraries',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: const Color(0xff303030),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Licence',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: const Color(0xff303030),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
