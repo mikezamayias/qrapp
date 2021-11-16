00010a61be94455f6d59cae422780e1aa61e5925
Mike Zamayias <mzamagias@gmail.com>
2021-11-16 02:50:37 +0200

    some messing around

--- lib/src/views/generate_page/generate_page.dart
+++ lib/src/views/generate_page/generate_page.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 // pub packages
 import 'package:page_transition/page_transition.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 // custom widgets
 import '../../widgets/page_blueprint.dart';
@@ -16,9 +17,11 @@ class GeneratePage extends StatefulWidget {
 }
 
 class _GeneratePageState extends State<GeneratePage> {
-  String data = 'QRapp';
+  TextEditingController _textEditingController = TextEditingController();
 
-  void _updateData(value) => setState(() => data = value);
+  String _data = 'QRapp';
+
+  void _updateData(value) => setState(() => _data = value);
 
   void _showQRCode(value) => Navigator.push(
         context,
@@ -40,38 +43,58 @@ class _GeneratePageState extends State<GeneratePage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(15),
-          child: TextFormField(
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w500,
-            ),
-            onFieldSubmitted: _showQRCode,
-            onChanged: _updateData,
-            cursorColor: const Color(0xff303030),
-            decoration: InputDecoration(
-              labelText: 'Data',
-              floatingLabelStyle: const TextStyle(
-                color: Color(0xFF303030),
-                fontSize: 21,
-                fontWeight: FontWeight.w600,
-                wordSpacing: 0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              QrImage(
+                data: _data,
+                version: QrVersions.auto,
+                size: MediaQuery.of(context).size.width * 0.72,
+                foregroundColor: const Color(0xff303030),
+                eyeStyle: QrEyeStyle(
+                  eyeShape: QrEyeShape.square,
+                  color: const Color(0xff303030),
+                ),
               ),
-              isDense: false,
-              errorMaxLines: 1,
-              errorText: null,
-              errorStyle: const TextStyle(
-                color: Colors.transparent,
-                fontSize: 0,
+              Spacer(),
+              TextFormField(
+                controller: _textEditingController,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+                onFieldSubmitted: _showQRCode,
+                onChanged: _updateData,
+                cursorColor: const Color(0xff303030),
+                decoration: InputDecoration(
+                  labelText: 'Data',
+                  floatingLabelStyle: const TextStyle(
+                    color: Color(0xFF303030),
+                    fontSize: 21,
+                    fontWeight: FontWeight.w600,
+                    wordSpacing: 0,
+                  ),
+                  isDense: false,
+                  errorMaxLines: 1,
+                  errorText: null,
+                  errorStyle: const TextStyle(
+                    color: Colors.transparent,
+                    fontSize: 0,
+                  ),
+                  alignLabelWithHint: true,
+                  focusColor: const Color(0xFF303030),
+                  labelStyle: const TextStyle(
+                    color: Color(0xFF303030),
+                  ),
+                  focusedBorder: stableStateBorder,
+                  border: stableStateBorder,
+                  enabledBorder: stableStateBorder,
+                ),
               ),
-              alignLabelWithHint: true,
-              focusColor: const Color(0xFF303030),
-              labelStyle: const TextStyle(
-                color: Color(0xFF303030),
-              ),
-              focusedBorder: stableStateBorder,
-              border: stableStateBorder,
-              enabledBorder: stableStateBorder,
-            ),
+              Spacer(flex: 2),
+            ],
           ),
         ),
       ),
