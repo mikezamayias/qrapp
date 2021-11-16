ff6a678f648dacea2acb8cdbbc270e9008152dd3
Mike Zamayias <mzamagias@gmail.com>
2021-11-16 02:11:53 +0200

    Removed about view in bottom sheet

--- lib/src/views/home_page/home_page.dart
+++ lib/src/views/home_page/home_page.dart
@@ -1,11 +1,7 @@
 // flutter packages
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-// pub packages
-import 'package:simple_gesture_detector/simple_gesture_detector.dart';
 
 // custom widgets
-import '../action_views/about_bottom_sheet_view.dart';
 import '../../widgets/bottom_nav_bar_buttons.dart';
 import '../generate_page/generate_page.dart';
 import '../scan_page/scan_page.dart';
@@ -61,38 +57,17 @@ class _HomePageState extends State<HomePage> {
           onPageChanged: _onPageChanged,
         ),
       ),
-      bottomNavigationBar: SimpleGestureDetector(
-        onVerticalSwipe: (direction) {
-          if (direction == SwipeDirection.up) {
-            HapticFeedback.lightImpact();
-            showModalBottomSheet(
-              isDismissible: false,
-              barrierColor: const Color(0xcc0303030),
-              // isScrollControlled: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(21)),
-              ),
-              context: context,
-              builder: (_) => AboutBottomSheetView(),
-            );
-          }
-        },
-        swipeConfig: const SimpleSwipeConfig(
-          verticalThreshold: 60,
-          horizontalThreshold: 60,
-        ),
-        child: BottomNavigationBar(
-          enableFeedback: true,
-          selectedItemColor: const Color(0xff303030),
-          unselectedItemColor: const Color(0x40303030),
-          currentIndex: _selectedNavIndex,
-          onTap: _onTap,
-          items: <BottomNavigationBarItem>[
-            generateNavButton(),
-            scanNavButton(),
-            aboutNavButton(),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        enableFeedback: true,
+        selectedItemColor: const Color(0xff303030),
+        unselectedItemColor: const Color(0x40303030),
+        currentIndex: _selectedNavIndex,
+        onTap: _onTap,
+        items: <BottomNavigationBarItem>[
+          generateNavButton(),
+          scanNavButton(),
+          aboutNavButton(),
+        ],
       ),
     );
   }
