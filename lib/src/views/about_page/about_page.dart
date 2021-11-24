3822cc8c7f1b338d24ecef655cbd961b460569f3
Mike Zamayias <mzamagias@gmail.com>
2021-11-24 17:17:19 +0200

    making about page

--- lib/src/views/about_page/about_page.dart
+++ lib/src/views/about_page/about_page.dart
@@ -11,6 +11,30 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return LicensePage();
+    return AboutDialog(
+      applicationIcon: Icon(
+        Icons.qr_code_rounded,
+        color: const Color(0xFF303030),
+      ),
+      applicationName: 'QRapp',
+      applicationVersion: '1.0.0',
+      applicationLegalese: 'MIT License',
+      children: <Widget>[
+        Text('This is a demo application for the Flutter framework.'),
+        Text('It is used to demonstrate the Flutter framework.'),
+        Text('It is used to demonstrate the Flutter framework.'),
+      ],
+    );
   }
 }
+
+/* 
+LicensePage(
+      applicationIcon: Icon(
+        Icons.qr_code_rounded,
+        color: const Color(0xFF303030),
+      ),
+      applicationName: 'QRapp!',
+      applicationVersion: '1.0.0',
+      applicationLegalese: 'MIT License',
+    ) */
