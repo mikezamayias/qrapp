a534fc18037827b8d8a09468aeb15a1ff6482818
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-02 01:05:42 +0200

    working on themes

--- lib/src/themes/themes.dart
+++ lib/src/themes/themes.dart
@@ -40,22 +40,22 @@ class CustomTheme with ChangeNotifier {
           color: Colors.transparent,
         ),
         focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: const Color(0xFF303030),
+          borderSide: const BorderSide(
+            color: Color(0xFF303030),
             width: 3,
             style: BorderStyle.solid,
           ),
           borderRadius: BorderRadius.circular(60),
         ),
         enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            color: const Color(0xFF303030),
+          borderSide: const BorderSide(
+            color: Color(0xFF303030),
             width: 3,
             style: BorderStyle.solid,
           ),
           borderRadius: BorderRadius.circular(60),
         ),
-        border: OutlineInputBorder(
+        border: const OutlineInputBorder(
           borderSide: BorderSide(
             color: Colors.blue,
             width: 3.0,
@@ -65,82 +65,77 @@ class CustomTheme with ChangeNotifier {
           gapPadding: 3.0,
         ),
       ),
-      appBarTheme: AppBarTheme(
+      appBarTheme: const AppBarTheme(
         iconTheme: IconThemeData(
-          color: const Color(0xFF8F6146),
+          color: Color(0xFF8F6146),
         ),
-        color: const Color(0xFFF3F3F3),
+        color: Color(0xFFF3F3F3),
         elevation: 3,
         centerTitle: true,
         titleTextStyle: TextStyle(
           fontWeight: FontWeight.bold,
           fontSize: 24,
-          color: const Color(0xFF8F6146),
+          color: Color(0xFF8F6146),
         ),
       ),
       shadowColor: const Color(0xff303030),
       primaryColor: const Color(0xFF8F6146),
       backgroundColor: const Color(0xfff3f3f3),
       scaffoldBackgroundColor: const Color(0xfff3f3f3),
-      iconTheme: IconThemeData(
-        color: const Color(0xFF8F6146),
+      iconTheme: const IconThemeData(
+        color: Color(0xFF8F6146),
         size: 24,
       ),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        elevation: 3,
-        backgroundColor: const Color(0xCCBE9279),
-        type: BottomNavigationBarType.fixed,
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        elevation: 0,
         enableFeedback: true,
         selectedIconTheme: IconThemeData(
-          color: const Color(0xFF8F6146),
+          color: Color(0xFF8F6146),
           size: 30,
         ),
-        unselectedIconTheme: IconThemeData(
-          color: const Color(0x99303030),
-          size: 30,
+        selectedLabelStyle: TextStyle(
+          color: Color(0xFF8F6146),
         ),
-        showUnselectedLabels: false,
-        showSelectedLabels: false,
       ),
-      textTheme: TextTheme(
+      textTheme: const TextTheme(
         headline1: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         headline2: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         headline3: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         headline4: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         headline5: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         headline6: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         subtitle1: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         subtitle2: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         bodyText1: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         bodyText2: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         caption: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         button: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
         overline: TextStyle(
-          color: const Color(0xff303030),
+          color: Color(0xff303030),
         ),
       ),
       textButtonTheme: TextButtonThemeData(
