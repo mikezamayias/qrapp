f05ba8b7c9c4bbce9b1679264ce3c6219ca2a41d
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-02 17:56:56 +0200

    working on the views

--- lib/src/views/about_page/about_page.dart
+++ lib/src/views/about_page/about_page.dart
@@ -1,6 +1,7 @@
 // flutter packages
 import 'package:flutter/material.dart';
 import 'package:qrapp/src/models/expansion_panel_section.dart';
+import 'package:qrapp/src/views/about_page/card_info.dart';
 import 'package:qrapp/src/widgets/page_blueprint.dart';
 
 class AboutPage extends StatefulWidget {
@@ -13,34 +14,33 @@ class AboutPage extends StatefulWidget {
 class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
-    return PageBlueprint(
+    return const PageBlueprint(
       title: 'About',
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          ExpansionPanelList(
-            expandedHeaderPadding: const EdgeInsets.all(9),
-            dividerColor: const Color(0xFF8F6146),
-            expansionCallback: (panelIndex, isExpanded) => setState(
-              () => expansionPanelSections[panelIndex].isExpanded = !isExpanded,
-            ),
-            children: [
-              for (final expansionPanelSection in expansionPanelSections)
-                ExpansionPanel(
-                  canTapOnHeader: true,
-                  isExpanded: expansionPanelSection.isExpanded ?? true,
-                  headerBuilder: (context, isExpanded) =>
-                      ListTile(title: expansionPanelSection.title),
-                  body: Padding(
-                    padding: const EdgeInsets.all(21.0),
-                    child: expansionPanelSection.body,
-                  ),
-                ),
-            ],
-          ),
-        ],
-      ),
+      body: CardInfo(),
+      // body: Column(
+      //   mainAxisSize: MainAxisSize.max,
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     ExpansionPanelList(
+      //       elevation: 0,
+      //       expandedHeaderPadding: const EdgeInsets.all(9),
+      //       dividerColor: const Color(0xFF8F6146),
+      //       children: [
+      //         for (final expansionPanelSection in expansionPanelSections)
+      //           ExpansionPanel(
+      //             canTapOnHeader: true,
+      //             isExpanded: expansionPanelSection.isExpanded ?? true,
+      //             headerBuilder: (context, isExpanded) =>
+      //                 ListTile(title: expansionPanelSection.title),
+      //             body: Padding(
+      //               padding: const EdgeInsets.fromLTRB(21, 12, 21, 12),
+      //               child: expansionPanelSection.body,
+      //             ),
+      //           ),
+      //       ],
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
---
+++ lib/src/views/about_page/card_info.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CardInfo extends StatelessWidget {
+  const CardInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(9),
+      child: Card(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(21),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const <Widget>[
+            ListTile(
+              leading: Icon(FontAwesomeIcons.code),
+              title: Text('App'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
