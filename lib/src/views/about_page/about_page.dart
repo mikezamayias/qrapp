fa62f13784bc2ba5fd96815ff9b17b64a4665d95
Mike Zamayias <29067825+mzamayias@users.noreply.github.com>
2022-01-06 09:29:47 +0200

    why is it so difficult to create the about page

--- lib/src/views/about_page/about_page.dart
+++ lib/src/views/about_page/about_page.dart
@@ -1,8 +1,6 @@
 // flutter packages
-import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
-import 'package:qrapp/src/views/about_page/panel_body.dart';
-import 'package:qrapp/src/views/about_page/panel_header.dart';
+import 'package:qrapp/src/models/expansion_panel_section.dart';
 import 'package:qrapp/src/widgets/page_blueprint.dart';
 
 class AboutPage extends StatefulWidget {
@@ -17,46 +15,33 @@ class _AboutPageState extends State<AboutPage> {
   Widget build(BuildContext context) {
     return PageBlueprint(
       title: 'About',
-      body: Padding(
-        padding: const EdgeInsets.all(9),
-        child: ExpandableNotifier(
-          child: Column(
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ExpansionPanelList(
+            elevation: 0,
+            expandedHeaderPadding: const EdgeInsets.all(9),
+            dividerColor: const Color(0xFF8F6146),
+            expansionCallback: (panelIndex, isExpanded) => setState(() {
+              expansionPanelSections[panelIndex].isExpanded = !isExpanded;
+            }),
             children: [
-              ClipRRect(
-                borderRadius: const BorderRadius.all(Radius.circular(21)),
-                child: ExpandablePanel(
-                  collapsed: ExpandableButton(child: const PanelHeader()),
-                  expanded: ExpandableButton(child: const PanelBody()),
+              for (final expansionPanelSection in expansionPanelSections)
+                ExpansionPanel(
+                  canTapOnHeader: true,
+                  isExpanded: expansionPanelSection.isExpanded!,
+                  headerBuilder: (context, isExpanded) =>
+                      ListTile(title: expansionPanelSection.title),
+                  body: Padding(
+                    padding: const EdgeInsets.fromLTRB(21, 12, 21, 12),
+                    child: expansionPanelSection.body,
+                  ),
                 ),
-              )
             ],
           ),
-        ),
+        ],
       ),
-      // body: Column(
-      //   mainAxisSize: MainAxisSize.max,
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   children: [
-      //     ExpansionPanelList(
-      //       elevation: 0,
-      //       expandedHeaderPadding: const EdgeInsets.all(9),
-      //       dividerColor: const Color(0xFF8F6146),
-      //       children: [
-      //         for (final expansionPanelSection in expansionPanelSections)
-      //           ExpansionPanel(
-      //             canTapOnHeader: true,
-      //             isExpanded: expansionPanelSection.isExpanded ?? true,
-      //             headerBuilder: (context, isExpanded) =>
-      //                 ListTile(title: expansionPanelSection.title),
-      //             body: Padding(
-      //               padding: const EdgeInsets.fromLTRB(21, 12, 21, 12),
-      //               child: expansionPanelSection.body,
-      //             ),
-      //           ),
-      //       ],
-      //     ),
-      //   ],
-      // ),
     );
   }
 }
